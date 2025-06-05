9005e1ff2ee9d04555432cb066c73dd06ecbaa2e
Praveen007-K <praveenknair2000@gmail.com>
2025-06-06 00:46:27 +0530

    Updated CreditHometab

--- app/src/main/java/com/pkoder/finest/presentation/screens/tabs/CreditHomeTab.kt
+++ app/src/main/java/com/pkoder/finest/presentation/screens/tabs/CreditHomeTab.kt
@@ -1,12 +1,15 @@
 package com.pkoder.finest.presentation.screens.tabs
 
+import android.widget.Toast
 import androidx.compose.foundation.layout.*
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.pkoder.finest.data.local.entities.CreditEntryEntity
+import com.pkoder.finest.data.local.entities.DebitEntryEntity
 import com.pkoder.finest.presentation.components.AmountTextField
 import com.pkoder.finest.presentation.components.DropdownField
 import com.pkoder.finest.presentation.viewmodel.FinanceViewModel
@@ -18,6 +21,7 @@ fun CreditScreen() {
 
     val incomeSources = listOf("Salary", "Freelance", "Gift", "Other")
     val viewModel: FinanceViewModel = hiltViewModel()
+    val context = LocalContext.current
 
     Column(Modifier.padding(16.dp)) {
         DropdownField("Income Source", incomeSources, incomeSource) {
@@ -33,12 +37,25 @@ fun CreditScreen() {
         Spacer(Modifier.height(16.dp))
 
         Button(onClick = {
-            viewModel.addCredit(
-                CreditEntryEntity(
-                    source = incomeSource,
-                    amount = amount.toDouble()
+            val dataNotEmpty : Boolean = incomeSource.isNotEmpty() && amount.isNotBlank()
+            if(dataNotEmpty){
+                viewModel.addCredit(
+                    CreditEntryEntity(
+                        source = incomeSource,
+                        amount = amount.toDouble()
+                    )
                 )
-            )
+
+                Toast.makeText(context, "Successfully added data.", Toast.LENGTH_SHORT).show()
+
+                //Clear fields after submit
+                incomeSource = ""
+                amount = ""
+
+            }
+            else{
+                Toast.makeText(context, "Fields cannot be empty!", Toast.LENGTH_SHORT).show()
+            }
         }) {
             Text("Submit")
         }
